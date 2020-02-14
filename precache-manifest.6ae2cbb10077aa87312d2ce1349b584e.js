de1266dd95b87a8bf0e4fa26acdd42df4a95ebc0
Armstrong Asenavi <zattubiz@gmail.com>
2020-02-14 20:58:32 +0300

    Updates

---
+++ precache-manifest.6ae2cbb10077aa87312d2ce1349b584e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40734f4a42dd60d0b3cfab4fad265b3",
+    "url": "/react_tutorial1/index.html"
+  },
+  {
+    "revision": "9231a38d115ffd28eb70",
+    "url": "/react_tutorial1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6edcdf994bf5c4113157",
+    "url": "/react_tutorial1/static/js/2.1ceb358c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_tutorial1/static/js/2.1ceb358c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9231a38d115ffd28eb70",
+    "url": "/react_tutorial1/static/js/main.d9672eaf.chunk.js"
+  },
+  {
+    "revision": "62f877e72a457ec35606",
+    "url": "/react_tutorial1/static/js/runtime-main.3e93439c.js"
+  }
+]);
